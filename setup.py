6a9afee7587885fc920ee83fb9fdbb763ebc6d5c
Angelo Dini <angelo.dini@divio.ch>
2018-12-07 14:26:47 +0100

    fix test matrix (#14)

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     license='BSD',
     description='Adds audio plugin to django CMS.',
     long_description=open('README.rst').read(),
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests']),
     include_package_data=True,
     zip_safe=False,
     install_requires=REQUIREMENTS,
