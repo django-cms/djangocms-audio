9be82126a199e53eb887830d7a12f50dcc9fb98b
Vinit Kumar <vinit.kumar@promobitech.com>
2026-03-23 22:24:02 +0530

    fix: upgrade python to be 3.10 plus for all repo code

--- aldryn_config.py
+++ aldryn_config.py
@@ -16,7 +16,7 @@ class Form(forms.BaseForm):
     )
 
     def clean(self):
-        data = super(Form, self).clean()
+        data = super().clean()
 
         # older versions of this addon had a bug where the values would be
         # saved to settings.json as a list instead of a string.
